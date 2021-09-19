0d07546aa39fcd61e47bc80bc5fb380a16606dff
Charaf <charaf4charaf@gmail.com>
2021-09-19 16:22:14 +0300

    fixed bugs

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (C) 2021 The Android Open Source Project.
  *
@@ -16,25 +17,24 @@
 
 package com.example.inventory
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import androidx.lifecycle.ViewModel
-import androidx.fragment.app.activityViewModels
 
 /**
  * [ItemDetailFragment] displays the details of the selected item.
  */
 class ItemDetailFragment : Fragment() {
-
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
     lateinit var item: Item
 
     private val viewModel: InventoryViewModel by activityViewModels {
@@ -42,35 +42,6 @@ class ItemDetailFragment : Fragment() {
             (activity?.application as InventoryApplication).database.itemDao()
         )
     }
-
-    private fun deleteItem() {
-        viewModel.deleteItem(item)
-        findNavController().navigateUp()
-    }
-
-    private fun editItem() {
-        val action = ItemDetailFragmentDirections.actionItemDetailFragmentToAddItemFragment(
-            getString(R.string.edit_fragment_title),
-            item.id
-        )
-        this.findNavController().navigate(action)
-    }
-
-    private fun bind(item: Item) {
-        binding.apply {
-            itemName.text = item.itemName
-            itemPrice.text = item.getFormattedPrice()
-            itemCount.text = item.quantityInStock.toString()
-            deleteItem.setOnClickListener { showConfirmationDialog() }
-            editItem.setOnClickListener { editItem() }
-        }
-
-        sellItem.isEnabled = viewModel.isStockAvailable(item)
-        sellItem.setOnClickListener { viewModel.sellItem(item) }
-
-    }
-
-    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
 
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
@@ -82,6 +53,32 @@ class ItemDetailFragment : Fragment() {
     ): View? {
         _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    /**
+     * Binds views with the passed in item data.
+     */
+    private fun bind(item: Item) {
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.getFormattedPrice()
+            itemCount.text = item.quantityInStock.toString()
+            sellItem.isEnabled = viewModel.isStockAvailable(item)
+            sellItem.setOnClickListener { viewModel.sellItem(item) }
+            deleteItem.setOnClickListener { showConfirmationDialog() }
+            editItem.setOnClickListener { editItem() }
+        }
+    }
+
+    /**
+     * Navigate to the Edit item screen.
+     */
+    private fun editItem() {
+        val action = ItemDetailFragmentDirections.actionItemDetailFragmentToAddItemFragment(
+            getString(R.string.edit_fragment_title),
+            item.id
+        )
+        this.findNavController().navigate(action)
     }
 
     /**
@@ -103,7 +100,20 @@ class ItemDetailFragment : Fragment() {
      * Deletes the current item and navigates to the list fragment.
      */
     private fun deleteItem() {
+        viewModel.deleteItem(item)
         findNavController().navigateUp()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = navigationArgs.itemId
+        // Retrieve the item details using the itemId.
+        // Attach an observer on the data (instead of polling for changes) and only update the
+        // the UI when the data actually changes.
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
     }
 
     /**
--- app/src/main/java/com/example/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/inventory/ItemListFragment.kt
@@ -55,26 +55,21 @@ class ItemListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val id = navigationArgs.itemId
-
-        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
-            item = selectedItem
-            bind(item)
-        }
-
         val adapter = ItemListAdapter {
-            val action = ItemListFragmentDirections.actionItemListFragmentToItemDetailFragment(it.id)
+            val action =
+                ItemListFragmentDirections.actionItemListFragmentToItemDetailFragment(it.id)
             this.findNavController().navigate(action)
         }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
         binding.recyclerView.adapter = adapter
-
+        // Attach an observer on the allItems list to update the UI automatically when the data
+        // changes.
         viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
             items.let {
                 adapter.submitList(it)
             }
         }
 
-        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
         binding.floatingActionButton.setOnClickListener {
             val action = ItemListFragmentDirections.actionItemListFragmentToAddItemFragment(
                 getString(R.string.add_fragment_title)
